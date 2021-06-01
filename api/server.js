ee36439a29ba79bb035ee8e6c4e2a8b8a06d6f1b
Ryder <ryderblue2@gmail.com>
2021-05-31 19:50:00 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,3 +1,22 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+// [GET] all users
+server.get('/api/users', async (req, res) => {
+    User.findAll()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                error: err.message});
+        });
+    
+})
+module.exports = server; // EXPORT YOUR SERVER instead of {}
